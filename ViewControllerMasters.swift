ed5ecce6787ab74a5b6f00a98f7ad767418d6056
Drew Knapp <39703673+yugedata@users.noreply.github.com>
2020-12-14 02:06:29 -0500

    Add files via upload

---
+++ ViewControllerMasters.swift
@@ -0,0 +1,36 @@
+//
+//  ViewControllerMasters.swift
+//  Pool
+//
+//  Created by Sato on 3/28/20.
+//  Copyright © 2020 Sato. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+
+class ViewControllerMasters: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
